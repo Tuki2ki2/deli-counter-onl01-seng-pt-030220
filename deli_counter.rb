85af4b1858a670f69c81796d657e911e763e6ed2
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-08 09:03:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 
 def take_a_number(katz_deli,name)
   if katz_deli.index(name) == nil
-    puts "Welcome, #{name}. You are."
+    puts "Welcome, #{name}. You are number 1 in line."
       elsif
       katz_deli.index(name)
       puts "Welcome, #{name}.You are number #{katz_deli.index(name)+1} in line"
